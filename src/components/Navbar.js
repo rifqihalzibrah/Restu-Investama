8fc79749b09ddc0f8fe19c995ff3a438dbfc0e8d
albanmuhammad <albanmuhammad07@gmail.com>
2022-11-03 16:44:43 +0700

    alban commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div>
+        <div className='navigation'>
             <nav className="navbar navbar-expand-lg bg-light">
                 <div className="container-fluid">
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">

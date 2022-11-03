2193525d3678eefdb11b5bda3a35cd57f0e601a5
rifqihalzibrah <rifqihalzibrahmuhammad@gmail.com>
2022-11-03 21:39:38 +0700

    Rifqih commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar.js';
+import About from './components/About/About.js';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      {/* <About /> */}
     </div>
   );
 }

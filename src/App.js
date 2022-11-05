ff8f22839d193bf8e8f6a63f5c0e738161054025
rifqihalzibrah <rifqihalzibrahmuhammad@gmail.com>
2022-11-05 16:26:23 +0700

    Rifqih commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 import './style.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/Navbar.js';
+import Navigation from './components/Navigation.js';
+import Home from './components/Home/Home.js'
 import About from './components/About/About.js';
+import Service from './components/Service/Service.js'
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      {/* <About /> */}
+      <BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/service' element={<Service />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Navigation.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function Navigation() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href='/'>RESTU INVESTAMA</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Item>
+              <Nav.Link as={Link} to='/'>Home</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link as={Link} to='/service'>Service</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link as={Link} to='/about'>About</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link as={Link} to='/contact'>Contact</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Navigation
